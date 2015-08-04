a5cddcbfbb338ec234031f83714496a8fb24c097
Christopher Brown <cbrown@bittorrent.com>
2015-08-03 20:39:02 -0700

    pep8 compliance in tests/run-all

--- tests/run-all.py
+++ tests/run-all.py
@@ -6,23 +6,23 @@ import shutil
 import subprocess
 import sys
 
-tests_directory = os.path.dirname(os.path.realpath(__file__))
-needy_path = os.path.join(tests_directory, '..', 'needy.py')
+TESTS_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
+NEEDY_PATH = os.path.join(TESTS_DIRECTORY, '..', 'needy.py')
 
 def test(directory, needy_args, run_dirty=False):
     print 'Running test in %s: %s' % (directory, ' '.join(needy_args))
     os.chdir(directory)
     if not run_dirty and os.path.exists('needs'):
         shutil.rmtree('needs')
-    subprocess.check_call([needy_path] + needy_args)
+    subprocess.check_call([NEEDY_PATH] + needy_args)
 
 def main(args):
     parser = argparse.ArgumentParser(description='Tests Needy.')
     parser.add_argument('--run-dirty', default=False, action='store_true', help='run without cleaning first (useful for saving time while debugging)')
     parameters = parser.parse_args(args[1:])
     
-    for entry in os.listdir(tests_directory):
-        test_directory = os.path.join(tests_directory, entry)
+    for entry in os.listdir(TESTS_DIRECTORY):
+        test_directory = os.path.join(TESTS_DIRECTORY, entry)
         if os.path.isfile(os.path.join(test_directory, 'needs.json')):
             test(test_directory, [], parameters.run_dirty)
             test(test_directory, ['--target=ios:armv7'], parameters.run_dirty)
