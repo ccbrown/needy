d2b323163bfffc1bce39d0213150bd25abaea98c
Christopher Brown <cbrown@bittorrent.com>
2016-04-23 06:03:27 -0700

    add project test for prebuild step

---
+++ tests/test_project.py
@@ -0,0 +1,17 @@
+import os
+from pyfakefs import fake_filesystem_unittest
+
+from needy.platform import host_platform
+from needy.project import Project, ProjectDefinition
+from needy.target import Target
+
+
+class ProjectTest(fake_filesystem_unittest.TestCase):
+    def setUp(self):
+        self.setUpPyfakefs()
+
+    def test_pre_build_creating_output_directories(self):
+        project = Project(ProjectDefinition(Target(host_platform()()), '.'), None)
+        project.pre_build('build/out')
+        self.assertTrue(os.path.isdir('./build/out/include'))
+        self.assertTrue(os.path.isdir('./build/out/lib'))
