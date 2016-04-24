33cb9c31947791b6449beeb76322344965570b3b
Christopher Brown <cbrown@bittorrent.com>
2016-04-24 01:49:12 -0700

    add url to setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
     name='needy',
     version='0.0',
     description='Dependency management utility.',
+    url='https://github.com/ccbrown/needy',
     long_description=read_file('README.md'),
     author='Christopher Brown',
     author_email='ccbrown112@gmail.com',
