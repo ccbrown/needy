883e407f64aecd73344177a095b0fe683f2d2842
Christopher Brown <cbrown@bittorrent.com>
2016-06-03 01:11:02 -0700

    better egg execution

--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python
 
 import os
+import zipfile
 
 from setuptools import setup
+from setuptools.command.bdist_egg import bdist_egg as _bdist_egg
+
+
+class bdist_egg(_bdist_egg):
+    def run(self):
+        _bdist_egg.run(self)
+        with open(self.egg_output, 'rb') as f:
+            data = f.read()
+        with open(self.egg_output, 'wb') as f:
+            f.write('#!/usr/bin/env python\n'.encode() + data)
+        z = zipfile.ZipFile(self.egg_output, 'a')
+        z.writestr('__main__.py', 'from needy.__main__ import main\nimport sys\nsys.exit(main(sys.argv))')
+        z.close()
 
 
 def read_file(path):
@@ -27,13 +41,11 @@ setup(
     entry_points={
         'console_scripts': [
             'needy = needy.__main__:main'
-        ],
-        'setuptools.installation': [
-            'eggsecutable = needy.__main__:main',
         ]
     },
     install_requires=[
         'colorama', 'jinja2', 'pyyaml'
     ],
+    cmdclass={'bdist_egg': bdist_egg},
     license='MIT'
 )
