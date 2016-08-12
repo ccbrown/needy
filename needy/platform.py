ce178799628f032709beb12346e7d447fd76e3c1
Victor Robertson <vmrob@bittorrent.com>
2016-08-12 14:47:57 -0700

    Add Project configuration hash

--- needy/platform.py
+++ needy/platform.py
@@ -1,4 +1,6 @@
 import sys
+import hashlib
+import json
 
 try:
     from exceptions import NotImplementedError
@@ -57,6 +59,26 @@ class Platform:
     def configuration_hash(self, architecture):
         """ returns a configuration hash in hex that can be used to detemine when a rebuild is necessary """
         return None
+
+    def configuration_hash(self, architecture):
+        cc = ''
+        cxx = ''
+
+        try:
+            cc = self.c_compiler(architecture)
+        except NotImplementedError:
+            pass
+        try:
+            cxx = self.cxx_compiler(architecture)
+        except NotImplementedError:
+            pass
+
+        hash = hashlib.sha256()
+        hash.update(json.dumps({
+            'c-compiler': cc,
+            'cxx-compiler': cxx,
+        }, sort_keys=True).encode())
+        return hash.digest()
 
     def default_architecture(self):
         """ returns the architecture to use if none is given """
